3187332c2079cb690aa98faf46ca7035b64d5c86
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2023-11-29 16:45:11 -0400

    No ramo main
    * Mudanças a serem submetidas:
    	modified:   composer.lock
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    shellscript.sh
    	modified:   tailwind.config.js
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>
    @rodrixcornell Rodrigo Cabral <rodrixcornell@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,49 +21,21 @@ export default {
   },
 
   plugins: [
-    forms,
-    typography,
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    // add daisyUI plugin
     require('daisyui'),
   ],
 
+  // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-    ],
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
     base: true, // applies background color and foreground color for root element by default
     styled: true, // include daisyUI colors and design decisions for all components
     utils: true, // adds responsive and modifier utility classes
-    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
   },
 };
